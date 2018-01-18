0f918c6d0422b92f0ecccf36851f0adc9910919b
Tatyana Scotce <tscotce@gmail.com>
2018-01-18 20:31:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,5 +35,5 @@ def draw?(board)
 end
 
 def over?(board)
-  won?(board) != false || draw?(board) != false || full?(board) != false
+  won?(board) == true || draw?(board) == true || full?(board) == true
 end
