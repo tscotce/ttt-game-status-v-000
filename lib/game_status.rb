23537c287e14e6808837d60bd34171a26ef4934c
Tatyana Scotce <tscotce@gmail.com>
2018-01-18 15:54:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     if board[win_combination[0]] != " " && board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]]
       return win_combination 
-    else puts false
+    else return false
     end
   end
 end
